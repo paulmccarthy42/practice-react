fd4afbda8d203b84cd42717a5b1bba51bab78c55
Paul McCarthy <paul.mccarthy@opternative.com>
2018-07-14 14:29:04 -0500

    make Product stateless

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,17 +1,11 @@
 import React, { Component } from 'react';
 
-export class Product extends Component {
-	constructor(props) {
-		super(props)
-	}
-
-	render() {
-		return (
-			<div>
-				<li>{this.props.product.name}</li>
-			</div>
-		);
-	}
+const Product = (props) => {
+	return (
+		<div>
+			<li>{props.product.name}</li>
+		</div>
+	);
 }
 
 export default Product;
