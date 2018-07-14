61f1a9efa6182de4dac45736e0b4277d50e972c9
Paul McCarthy <paul.mccarthy@opternative.com>
2018-07-14 15:02:34 -0500

    cleanup and add elements to product comp

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react';
+import './Product.css'
 
 const Product = (props) => {
+	const prodInfo = props.product
+
 	return (
+		<div className='product'>
 		<div>
-			<li>{props.product.name}</li>
+			<img src={prodInfo.img} alt= {prodInfo.name}/>
 		</div>
+		<div>
+			<h4 className='product-name'>{prodInfo.name}</h4>
+			<div className='product-info'>
+				<div>
+					<p> by {prodInfo.seller} </p>
+					<p> ${prodInfo.price} </p>
+					<button>Add to cart</button>
+					<div className='features'>Features</div>
+				</div>
+			</div>
+		</div>
+	</div>
 	);
 }
 
